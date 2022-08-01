c5bc431bc41c0bd905136e776ad3f06a68b7affb
top80276 <top80276@gmail.com>
2022-08-01 11:30:31 +0800

    Add blur programs

---
+++ blur_bilateral.py
@@ -0,0 +1,20 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+    help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+# stack output images together
+blurred = np.hstack([
+    cv2.bilateralFilter(image, 5, 21, 21),
+    cv2.bilateralFilter(image, 7, 31, 31),
+    cv2.bilateralFilter(image, 9, 41, 41)])
+
+cv2.imshow("Bilateral", blurred)
+cv2.waitKey(0)
